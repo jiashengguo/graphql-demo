92951646655fb3d2738fea38de846920dc851077
JG <jiashengguo@outlook.com>
2023-10-24 23:56:44 +0100

    add ZenStack

--- test.ts
+++ test.ts
@@ -1,7 +1,6 @@
-import { initTRPC } from "@trpc/server";
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
-import * as z from "zod";
-
+/**
+ * local version
+ */
 export type Post = { id: number; title: string };
 
 export type User = { id: number; name: string };
@@ -24,8 +23,12 @@ const p1 = loadPost({ id: 1 });
 const p2 = loadPost({ id: 1, withAuthor: true });
 
 /**
- * tRPC
+ * tRPC version
  */
+
+import { initTRPC } from "@trpc/server";
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import * as z from "zod";
 const t = initTRPC.create();
 
 export const router = t.router;
@@ -48,3 +51,25 @@ const trpc = createTRPCProxyClient<AppRouter>({
 
 const t1 = await trpc.loadPost.query({ id: 1 });
 const t2 = await trpc.loadPost.query({ id: 1, withAuthor: true });
+
+/**
+ * ZenStack tRPC version
+ */
+import { createRouter } from "./src/server/routers/generated/routers";
+import { createTRPCReact } from "./src/server/routers/generated/client/react";
+
+export const zRouter = createRouter(t.router, t.procedure);
+
+export type ZRouter = typeof zRouter;
+
+const zRPC = createTRPCReact<ZRouter>({});
+
+const z1 = zRPC.post.findFirst.useQuery({
+    where: { id: "1" },
+    select: { id: true, title: true },
+}).data;
+
+const z2 = zRPC.post.findFirst.useQuery({
+    where: { id: "1" },
+    select: { id: true, title: true, author: true },
+}).data;
